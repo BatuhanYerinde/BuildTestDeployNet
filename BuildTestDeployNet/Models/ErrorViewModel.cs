dd4cbf880b823ce6aedf3e10b27ab11b85e27e32
$(Build.RequestedFor) <$(Build.RequestedForEmail)>
2025-02-09 19:09:47 +0300

    first commit

---
+++ BuildTestDeployNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuildTestDeployNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
